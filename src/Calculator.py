8a626d9ddbb2f54f89fb6cfacb40bad4e77ea771
tanvi3110 <tanvishah3110@gmail.com>
2021-07-04 00:48:55 -0400

    Feature: setup calculator with multiplication method

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,9 +1,25 @@
-def addition(a, b):
-    return a + b
+from CsvReader import CsvReader
 
 
 def subtraction(a, b):
-    return a - b
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+
+def addition(a, b):
+    a = int(a)
+    b = int(b)
+    c = a + b
+    return c
+
+
+def multiplication(a, b):
+    a = int(a)
+    b = int(b)
+    c = a * b
+    return c
 
 
 class Calculator:
@@ -12,12 +28,22 @@ class Calculator:
     def __init__(self):
         pass
 
-    def add(self, a, b):
-        self.result = addition(a, b)
-        return self.result
-
     def subtract(self, a, b):
         self.result = subtraction(a, b)
         return self.result
 
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
 
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+
+class CSVStats(Calculator):
+    data = []
+
+    def __init__(self, data_file):
+        self.data = CsvReader(data_file)
+        pass
---
+++ src/CalculatorTests.py
@@ -0,0 +1,36 @@
+import unittest
+from Calculator import Calculator
+from CsvReader import CsvReader
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_subtraction(self):
+        test_data1 = CsvReader('/src/subtraction.csv').data
+        for row in test_data1:
+            self.assertEqual(self.calculator.subtract(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_addition(self):
+        test_data1 = CsvReader('/src/addition.csv').data
+        for row in test_data1:
+            self.assertEqual(self.calculator.add(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_multiplication(self):
+        test_data = CsvReader('/src/multiplication.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.multiply(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
