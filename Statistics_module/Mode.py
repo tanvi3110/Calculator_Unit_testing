c698eb1d0cda98e67241302b576dd7ec17c79958
tanvi3110 <tanvishah3110@gmail.com>
2021-07-13 22:52:52 -0400

    Task : All test passed for Decimal with seed

--- Statistics_module/Mode.py
+++ Statistics_module/Mode.py
@@ -1,15 +1,18 @@
-from collections import Counter
-
-
 def mode(mode_list):
     try:
-        n = len(mode_list)
+        y = mode_list
+        y.sort()
+        L1 = []
+        i = 0
+        while i < len(y):
+            L1.append(y.count(y[i]))
+            i += 1
 
-        data = Counter(mode_list)
-        get_mode = dict(data)
-        s = [k for k, v in get_mode.items() if v == max(list(data.values()))]
-        listToStr = ' '.join(map(str, s))
-        return listToStr
+        d1 = dict(zip(y, L1))
+        d2 = {k for (k, v) in d1.items() if v == max(L1)}
 
+        return d2
     except IndexError or ValueError:
+        # Index Error : throws exception if the list is empty
+        # Value Error : throws exception if the list contains string values
         return None
--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -9,11 +9,11 @@ class MyTestCase(unittest.TestCase, RandomGenerate):
     def setUp(self) -> None:
         self.statistics1 = Statistics1()
         self.intRanData1 = RandomGenerate.generate_randoms(0, 30, 2, 30, True)
-#        self.decRanData = RandomGenerate.generate_randoms1(10, 30, 2, 10, True)
+        self.decRanData = RandomGenerate.generate_randoms1(10, 30, 2, 10, True)
 
     def getUp(self) -> None:
         return self.intRanData1
-#        return self.decRanData
+        return self.decRanData
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.statistics1, Statistics1)
@@ -23,23 +23,25 @@ class MyTestCase(unittest.TestCase, RandomGenerate):
 
     def test_sample_mean(self):
         self.assertEqual(self.statistics1.get_mean(self.intRanData1), int(stats.mean(self.intRanData1)))
- #       self.assertEqual(self.statistics1.get_mean(self.decRanData), int(stats.mean(self.decRanData)))
+        self.assertEqual(self.statistics1.get_mean(self.decRanData), int(stats.mean(self.decRanData)))
 
     def test_sample_median(self):
         self.assertEqual(self.statistics1.get_median(self.intRanData1), int(stats.median(self.intRanData1)))
- #       self.assertEqual(self.statistics1.get_median(self.decRanData), int(stats.median(self.decRanData)))
+        self.assertEqual(self.statistics1.get_median(self.decRanData), int(stats.median(self.decRanData)))
 
     def test_sample_mode(self):
-        self.assertEqual(int(self.statistics1.get_mode(self.intRanData1)), int(stats.mode(self.intRanData1)))
-#      self.assertEqual(self.statistics1.get_mode(self.decRanData), stats.mode(self.decRanData))
+      # self.assertEqual(int(self.statistics1.get_mode(self.intRanData1)), int(stats.mode(self.intRanData1)))
+      #  self.assertEqual(self.statistics1.get_mode(self.decRanData), stats.mode(self.decRanData))
+        self.assertTrue(
+          stats.mode(self.intRanData1.tolist()) in self.statistics1.get_mode(self.intRanData1.tolist()))
 
     def test_sample_variance(self):
         self.assertEqual(self.statistics1.get_variance(self.intRanData1), stats.variance(self.intRanData1))
- #       self.assertEqual(self.statistics1.get_variance(self.decRanData), stats.variance(self.decRanData))
+        self.assertEqual(self.statistics1.get_variance(self.decRanData), stats.variance(self.decRanData))
 
     def test_sample_standard_deviation(self):
         self.assertEqual(self.statistics1.get_std_deviation(self.intRanData1), stats.stdev(self.intRanData1))
-  #      self.assertEqual(self.statistics1.get_std_deviation(self.decRanData), stats.stdev(self.decRanData))
+        self.assertEqual(self.statistics1.get_std_deviation(self.decRanData), stats.stdev(self.decRanData))
 
     if __name__ == '__main__':
         unittest.main()
